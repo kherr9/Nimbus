abe2c317c31adc306287bf3f541215ac4c16738f
Kevin Herr <kherr9@gmail.com>
2014-11-06 16:41:25 -0600

    first commit of branch

--- src/Nimbus/FodyTests.cs
+++ src/Nimbus/FodyTests.cs
@@ -5,7 +5,7 @@ namespace Nimbus
     /// <summary>
     ///     We DELIBERATELY do not explicitly null-check any arguments in this codebase - it's too easy to forget. We
     ///     use Fody and apply it across the board. This is a test class (which needs to be in the core Nimbus assembly)
-    ///     that allows our test suite to confirm that Fody is correctly injecting its NullGuard aspect.
+    ///     that allows our test suite to confirm that Fody is correctly injecting its NullGuard aspect. 
     /// </summary>
     public class FodyTests
     {
